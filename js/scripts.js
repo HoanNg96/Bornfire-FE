6e17b208244b9ad01b497653f5368b9a4abcc366
HoanNg96 <nhhoan96@gmail.com>
2021-08-17 16:01:51 +0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $('.menu-icon').click(function (e) { 
+        e.preventDefault();
+        $('.menu-bar').addClass('active');
+        $('.cover').addClass('active');
+    });
+    $('.cover').click(function (e) { 
+        e.preventDefault();
+        $('.menu-bar').removeClass('active');
+        $('.cover').removeClass('active');
+    });
+    $('.slider-social li').click(function (e) { 
+        e.preventDefault();
+        $(this).find('.item').addClass('active');
+        $(this).siblings().find('.item').removeClass('active');
+    });
+    $('.social .item').click(function (e) { 
+        e.preventDefault();
+        $(this).addClass('active');
+        $(this).siblings().removeClass('active');
+    });
+    $('.nav-item').click(function (e) { 
+        e.preventDefault();
+        $(this).addClass('active');
+        $(this).siblings().removeClass('active');
+    });
+});
